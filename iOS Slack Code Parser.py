4bb7152f122e1564ab559d067b85ec938d9c4270
Peteous <erik.peterman@und.edu>
2017-01-17 13:54:42 -0600

    Add comments to the script

--- iOS Slack Code Parser.py
+++ iOS Slack Code Parser.py
@@ -4,8 +4,13 @@ import clipboard
 #Example Input:
 #000000000111111111222222222333333333444444444555555555666666666777777777888888888999999999
 
+#Establish an array of 90 characters from the clipboard
 array = list(clipboard.get())
+
+#Define the length of each recovery code as 9 characters, which is what Slack uses
 codeLen = 9
+
+#Iterate through the 90 copied characters from thr clipboard array, inserting spaces after every 9 characters are parsec
 for index in range(90):
 	if index <= 8:
 		if (index + 1)%codeLen == 0:
@@ -15,10 +20,14 @@ for index in range(90):
 		array.insert(index+1,' ')
 		index += 1
 
+#Establish a blank string which can be added to based upon the adjusted array made in previous step
 string = ''
+
+#Add the characters from the array to the string, covering all 99 characters
 for index in range(99):
 	string += array[index]
 
+#Save the string to the clipboard
 clipboard.set(string)	
 
 #Example Output:
