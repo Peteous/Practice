7143bd9fdffec690663adfbd4fbd70ae279bf5f9
Peteous <erik.peterman@und.edu>
2017-01-20 21:29:27 -0600

    define new internal method for character shifts

--- Encode.py
+++ Encode.py
@@ -1,36 +1,42 @@
-def urlEncode(self,URL,paramDict=None):
-	if not paramDict == None:
-		dictKeys = paramDict.keys()
-		length = len(dictKeys)
-		for index in range(length):	
-			dictVars[index] = paramDict[dictKeys[index]]
-			output = '?'
-		for i in range(length):
-			if i < 1:
-				output += dictKeys[i] + '=' + dictVars[i]
-			else:
-				output += '&' + dictKeys[i] + '=' + dictVars[i]
-		output = str(URL)
-		output.replace('%','%25')
-		output.replace('!','%21')
-		output.replace('#','%23')
-		output.replace('$','%24')
-		output.replace('&','%26')
-		output.replace('\'','%27')
-		output.replace('(','%28')
-		output.replace(')','%29')
-		output.replace('*','%2A')
-		output.replace('+','%2B')
-		output.replace(',','%2C')
-		output.replace('/','%2F')
-		output.replace(':','%3A')
-		output.replace(';','%3B')
-		output.replace('=','%3D')
-		output.replace('?','%3F')
-		output.replace('@','%40')
-		output.replace('[','%5B')
-		output.replace(']','%5D')
-		return output
+def urlEncode(self,url,apiURL=None,paramDict=None):
+	url = urlCharShift(url)
+	if not apiURL == None:
+		if not paramDict == None:
+			dictKeys = urlCharShift(paramDict.keys())
+			length = len(dictKeys)
+			for index in range(length):	
+				dictVars[index] = urlCharShift(paramDict[dictKeys[index]])
+				output = '?'
+			for i in range(length):
+				if i < 1:
+					output += dictKeys[i] + '=' + dictVars[i]
+				else:
+					output += '&' + dictKeys[i] + '=' + dictVars[i]
+			return output
+		else:
+	else: 
 def write(self,URL,encodedStr):
 	output = URL + '?' + encodedStr
 	return output
+def urlCharShift(self,text):
+	url = str(text)
+	url.replace('%','%25')
+	url.replace('!','%21')
+	url.replace('#','%23')
+	url.replace('$','%24')
+	url.replace('&','%26')
+	url.replace('\'','%27')
+	url.replace('(','%28')
+	url.replace(')','%29')
+	url.replace('*','%2A')
+	url.replace('+','%2B')
+	url.replace(',','%2C')
+	url.replace('/','%2F')
+	url.replace(':','%3A')
+	url.replace(';','%3B')
+	url.replace('=','%3D')
+	url.replace('?','%3F')
+	url.replace('@','%40')
+	url.replace('[','%5B')
+	url.replace(']','%5D')
+	return url
