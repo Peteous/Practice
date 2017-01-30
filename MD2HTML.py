7046065e5686d23316606ce6e6652aa24370592f
Peteous <erik.peterman@und.edu>
2017-01-30 17:10:13 -0600

    Fixed compile errors

--- MD2HTML.py
+++ MD2HTML.py
@@ -1,22 +1,31 @@
 import re
-from Encode.py import htmlEncode
+from Encode import htmlEncode
 
 _text = r"[\S]*"
 _link = r"http[s]?://"
 _linkBrackets = r"["+_text+r"]("+_link+r")"
 
 def urlParse(text):
-    mdLink = re.findall(_linkBrackets)
-    if mdLink:
-        ahref = str(re.findall(r"("+_link+r")",mdLink))
-        ahref.strip(['(',')'])
-        ahref = htmlEncode(ahref)
-        ahref = '<a href='+'\"'+ahref+'\">'
-        p = re.findall(r"["+_text+r"]",mdLink)
-        p.strip(['[',']'])
-        p = p+r"</a>"
-        p = ahref + p
-        return '<p>' + p + '</p>'
-    else:
-        print('No Markdown Links Found')
-        return ''
+	mdLink = re.findall(_linkBrackets,text)
+	if not mdLink == None:
+		ahref = str(re.findall(r"("+_link+r")",str(mdLink)))
+		ahref = ahref.strip('()')
+		ahref = htmlEncode(ahref)
+		ahref = '<a href='+'\"'+ahref+'\">'
+		p = str(re.findall(r"["+_text+r"]",str(mdLink)))
+		p.strip('()')
+		p = p+r"</a>"
+		p = ahref + p
+		return '<p>' + p + '</p>'
+	else:
+		print('No Markdown Links Found')
+		return ''
+
+def main():
+	import clipboard
+	string = clipboard.get()
+	string = urlParse(string)
+	print(string)
+	
+if __name__ == "__main__":
+	main()
