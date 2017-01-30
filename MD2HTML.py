faaf09eb484ec72444d35276f2d7b7e36c3582e3
Peteous <erik.peterman@und.edu>
2017-01-30 13:16:41 -0600

    Flesh out urlParse Method

--- MD2HTML.py
+++ MD2HTML.py
@@ -1,7 +1,20 @@
 import re
 
 _text = r"[\S]*"
-_linkBrackets = r"\["+_text+r"\]\("+_text+r"\)"
+_link = r"http[s]?://"
+_linkBrackets = r"["+_text+r"]("+_link+r")"
 
 def urlParse(text):
-    
+    mdLink = re.findall(_linkBrackets)
+    if mdLink:
+        ahref = str(re.findall(r"("+_link+r")",mdLink))
+        ahref.strip(['(',')'])
+        ahref = '<a href='+'\"'+ahref+'\">'
+        p = re.findall(r"["+_text+r"]",mdLink)
+        p.strip(['[',']'])
+        p = p+r"</a>"
+        p = ahref + p
+        return '<p>' + p + '</p>'
+    else:
+        print('No Markdown Links Found')
+        return ''
