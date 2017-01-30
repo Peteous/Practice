f0827e01bb97a87b0b11eeba7850395a33859ebd
Peteous <erik.peterman@und.edu>
2017-01-30 13:33:30 -0600

    Update internal method syntax

--- Encode.py
+++ Encode.py
@@ -7,68 +7,68 @@
 def urlEncode(url,apiURL=None,paramNames=None,paramData=None):
 	#Establish an initial null output
 	output = ''
-	
+
 	#Encode the input url, and then add the expected url parameter before it
-	url = urlCharShift(url)
+	url = _urlCharShift(url)
 	url = 'url='+url
-	
+
 	#Logic for parameters
 	#	if there are parameters to encode and an apiURL
 	if not paramNames == None and not paramData == None and not apiURL == None:
 		#start the output with the apiURL, since that doesn't need encoding, and add a ?
 		output += apiURL
 		output += '?'
-		
+
 		#find length of the paramNames list for iteration
 		length = len(paramNames)
-		
+
 		#iterate through the paramNames list and paramData list, encoding paramData values
 		for index in range(length):
-			paramData[index] = urlCharShift(paramData[index])
+			paramData[index] = _urlCharShift(paramData[index])
 			#choose seperate character encoding sequences if it's the first value or not
 			if index < 1:
 				output += paramNames[index] + '=' + paramData[index]
 			else:
 				output += '&' + paramNames[index] + '=' + paramData[index]
-		
+
 		#add & character to end of string, then add the encoded url value, and return output
 		output += '&'
 		output += url
 		return output
-		
+
 	#	if there are not parameters, but there is an apiURL
 	elif paramNames == None and not apiURL == None:
 		output += apiURL
 		output += '?'
 		output += url
 		return output
-		
+
 	# if there isn't an apiURL but there is paramNames and paramData
 	elif apiURL == None and not paramNames == None and not paramData == None:
 		#follow steps of first if statement, but skip encoding the apiURL
 		length = len(paramNames)
 		for index in range(length):
-			paramData[index] = urlCharShift(paramData[index])
+			paramData[index] = _urlCharShift(paramData[index])
 			if index < 1:
 				output += paramNames[index] + '=' + paramData[index]
 			else:
 				output += '&' + paramNames[index] + '=' + paramData[index]
 		output += url
 		return output
-	
-	#	if there is just a url	
+
+	#	if there is just a url
 	elif apiURL == None and paramNames == None and paramData == None:
 		output += url
 		return output
-		
+
 	#	Handle edge cases
 	else:
 		print('\n\nAn error occured. Please check your input parameters.')
 		return output
 	#End logic for parameters
-	
+
 #internal method containing character rules for url encoding
-def urlCharShift(text):
+def _urlCharShift(text):
 	url = str(text)
 	url.replace('%','%25')
 	url.replace('!','%21')
