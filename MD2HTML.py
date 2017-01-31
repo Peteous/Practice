6b81597c8971dac1f5bb068ed875612fe801c710
Peteous <erik.peterman@und.edu>
2017-01-31 10:50:19 -0600

    Switch regex to string parsing to fix bugs

--- MD2HTML.py
+++ MD2HTML.py
@@ -1,23 +1,28 @@
-import re
 from Encode import htmlEncode
 
-_text = r"[\S]*"
-_link = r"http[s]?://"
-_linkBrackets = "\[.+?\]\(^https?:\/\/.+?\)"
-
 def urlParse(text):
-	mdLink = re.findall(_linkBrackets,text)
-	print(mdLink)
-	if not mdLink == None:
-		ahref = str(re.findall(r"("+_link+r")",str(mdLink)))
-		ahref = ahref.strip('()')
-		ahref = htmlEncode(ahref)
-		ahref = '<a href='+'\"'+ahref+'\">'
-		p = str(re.findall(r"["+_text+r"]",str(mdLink)))
-		p.strip('()')
-		p = p+r"</a>"
-		p = ahref + p
-		return '<p>' + p + '</p>'
+	link = ''
+	title = ''
+	text = list(text)
+	for char in text:
+		if char == '[':
+			start = text.index(char)
+		if char == ']':
+			mid1 = text.index(char)
+		if char == '(':
+			mid2 = text.index(char)
+		if char == ')':
+			end = text.index(char)
+	for index in range(len(text)):
+		if index > start and index < mid1:
+			title += text[index]
+		if index > mid2 and index < end:
+			if text[index]=='&':
+				link += '&amp;'
+			else:
+				link += text[index]
+	if not link == '':
+		return '<p>'+'<a href='+'\"'+link+'\">'+title+'</a></p>'
 	else:
 		print('No Markdown Links Found')
 		return ''
