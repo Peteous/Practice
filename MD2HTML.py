8a0f15739ad680d819b16c65cb6e602195728a43
Peteous <erik.peterman@und.edu>
2017-01-31 14:35:13 -0600

    Added bold parsing method

--- MD2HTML.py
+++ MD2HTML.py
@@ -89,6 +89,37 @@ def underParse(text):
 			print('No underscores found')
 			return ''
 
+def boldParse(text):
+	text = list(text)
+	output = ''
+	__first = 0
+	for char in text:
+		if char == '_' and __first == 0:
+			if text[text.index(char)+1] == '_':
+				__start = text.index(char)
+				__first == 1
+			#Escape case for finding bold-marked text
+			else:
+				__first = 2
+		if char == '_' and __first == 1:
+			if text[text.index(char)+1] == '_':
+				__end = text.index(char)
+				__first == 2
+			else:
+				__first = 2
+	if __start == None or __end == None:
+		print('No double asterisks found')
+		return ''
+	else:
+		for index in range(len(text)):
+			if index > __start+1 and index < __end:
+				output += text[index]
+		if not output == '':
+			return '<strong>'+output+'</strong>'
+		else:
+			print('No double asterisks found')
+			return ''
+
 def main():
 	import clipboard
 	string = clipboard.get()
