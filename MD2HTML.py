a0607b1dfe59b0a79656dbc66b34fdebe2422b31
Peteous <erik.peterman@und.edu>
2017-02-02 13:11:47 -0600

    implemented private generic _parse method in one public method

--- MD2HTML.py
+++ MD2HTML.py
@@ -27,35 +27,7 @@ def urlParse(text):
 
 #Does not handle literal asterisks \*
 def italicsParse(text):
-	text = list(text)
-	output = ''
-	__first = 0
-	for char in text:
-		if char == '*' and __first == 0:
-			if not text[text.index(char)+1] == '*':
-				__start = text.index(char)
-				__first == 1
-			#Escape case for finding bold-marked text
-			else:
-				__first = 2
-		if char == '*' and __first == 1:
-			if not text[text.index(char)+1] == '*':
-				__end = text.index(char)
-				__first == 2
-			else:
-				__first = 2
-	if __start == None or __end == None:
-		print('No italics found')
-		return ''
-	else:
-		for index in range(len(text)):
-			if index > __start and index < __end:
-				output += text[index]
-		if not output == '':
-			return '<em>'+output+'</em>'
-		else:
-			print('No italics found')
-			return ''
+	return _parse(text,'*',1,'<em>')
 
 #does not handle literal underscores \_
 def underParse(text):
@@ -122,6 +94,9 @@ def boldParse(text):
 
 def _parse(text,char,num,tag):
 	text = list(text)
+	char = char(char)
+	num = int(num)
+	tag = str(tag)
 	output = ''
 	__endtag = '</'+tag.strip('<>')+'>'
 	__first = 0
